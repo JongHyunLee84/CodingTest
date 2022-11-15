7432a714d499dcebf0a817853ec33b14267bcc41
JongHyunLee84 <112399028+JongHyunLee84@users.noreply.github.com>
2022-11-16 08:50:39 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.08 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.swift
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.swift
@@ -1,6 +1,5 @@
 import Foundation
 
 func solution(_ array:[Int]) -> Int {
-    var result = array.sorted()
-    return result.count % 2 == 0 ? result[(result.count / 2) - 1] : result[(result.count / 2)]
+    return array.sorted()[array.count / 2]
 }
