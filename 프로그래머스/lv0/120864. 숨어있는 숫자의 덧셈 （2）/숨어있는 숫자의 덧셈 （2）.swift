800042d165c6d0cc0629bd01ff738864786d9723
JongHyunLee84 <112399028+JongHyunLee84@users.noreply.github.com>
2022-11-26 23:15:55 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.14 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.swift
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.swift
@@ -1,20 +1,8 @@
 import Foundation
 
-func solution(_ my_string:String) -> Int {
-    var result = 0 
-    var str = "0"
-    var array = Array<Int>()
-    for i in my_string {
-        if i.isNumber {
-            str += String(i)
-        } else {
-            array.append(Int(str)!)
-            str = "0"
-        }
-    }
-    array.append(Int(str)!)
-    for i in array {
-        result += i
-    }
-    return result
+func solution(_ my_string:String) -> Int{
+    let a = my_string.map{ $0.isNumber ? String($0) : " " }
+     return a.joined().components(separatedBy: " ").filter{$0 != ""}.map{ Int($0)! }.reduce(0, +)
+        
 }
+
