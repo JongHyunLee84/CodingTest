28e7d5863dcd23d323be912eb551bd07dc1dd0dc
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2022-12-14 21:23:17 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.swift
@@ -0,0 +1,11 @@
+func solution(_ n:Int) -> String {
+    var result = "" 
+    for i in 1...n {
+        if i % 2 != 0 {
+            result += "수"
+        } else {
+            result += "박"
+        }
+    }
+    return result 
+}
