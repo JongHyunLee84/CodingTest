6b5b1de12da04fbcf85acd743dd7c1b449ea1f43
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2022-12-17 16:23:35 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.04 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.swift
+++ 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.swift
@@ -1,14 +1,5 @@
 import Foundation
 
 func solution(_ array:[Int], _ n:Int) -> Int {
-    var result = 0
-    // 배열의 요소들과 n을 비교
-    for i in array {
-        if i == n {
-            result += 1
-        }
-    }
-    // n과 같을 때 갯수를 셈
-    // 갯수를 리턴
-    return result
+    array.filter { $0 == n }.count
 }
