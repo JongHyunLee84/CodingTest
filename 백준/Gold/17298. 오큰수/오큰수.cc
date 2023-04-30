bf749dfb4cc4631634229d04b5effe2cedae7ab8
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-04-30 20:20:37 +0900

    [Gold IV] Title: 오큰수, Time: 256 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, ar[1000004], ret[1000004], t;
+stack<int> s;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    memset(ret, -1, sizeof(ret));
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> ar[i];
+        while (s.size() && ar[s.top()] < ar[i])
+        {
+            ret[s.top()] = ar[i];
+            s.pop();
+        }
+        s.push(i);
+    }
+    for (int i = 0; i < n; i++)
+        cout << ret[i] << ' ';
+    return 0;
+}
