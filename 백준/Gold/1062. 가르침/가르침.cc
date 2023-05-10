bbc6e9215ff668f6e7a389066b0215096f7f98c6
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-05-10 22:55:09 +0900

    [Gold IV] Title: 가르침, Time: 28 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.cc
+++ 백준/Gold/1062. 가르침/가르침.cc
@@ -4,14 +4,14 @@ int n, m, words[51];
 string s; 
 int count(int mask) {
     int cnt = 0;
-    for (int i=0; i<n; i++) { 
-        if((words[i] & mask) == words[i])cnt++; 
+    for (int word : words) { 
+        if(word && (word & mask) == word)cnt++; 
     }
     return cnt;
 }
 int go(int index, int k, int mask) {
-    if (k < 0) return 0;
-    if (index == 26) return count(mask); 
+    if (k == 0) return count(mask);
+    if (index >= 26) return 0; 
     int ret = go(index+1, k-1, mask | (1 << index)); 
     if (index != 'a'-'a' && index != 'n'-'a' && index != 't'-'a' && index != 'i'-'a' && index != 'c'-'a') {
         ret = max(ret, go(index+1, k, mask)); 
