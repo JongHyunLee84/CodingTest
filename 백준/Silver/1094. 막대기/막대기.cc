935d9671c18f1a71b1f5560a830361578cb5c85f
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-05-10 23:49:27 +0900

    [Silver V] Title: 막대기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.cc
+++ 백준/Silver/1094. 막대기/막대기.cc
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-int x, cnt;
-
-int main() {
-    cin >> x;
-    for(int i = 0; i<= 7; i++){
-        if(x & (1<<i))cnt++;
-    }
-    cout << cnt << '\n';
+int n, ret = 0;
+int main(){ 
+	cin >> n; 
+	while(n != 0){
+        if(n & 1) ret++; 
+        n /= 2;
+	}
+	cout << ret << '\n';
     return 0;
 }
