6c526ebea67548b9d5b115885e21859ca6f28767
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-05-12 20:19:18 +0900

    [Silver III] Title: 큰 수 A+B (2), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15353. 큰 수 A＋B （2）/큰 수 A＋B （2）.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string a, b, ret;
+
+int main(){
+    cin >> a >> b;
+    int sum = 0;
+    while(a.size() || b.size() || sum){
+        // cout << "here" << '\n';
+        if(a.size()){
+            sum += a.back() - '0';
+            a.pop_back();
+        }
+        if(b.size()){
+            sum += b.back() - '0';
+            b.pop_back();
+        }
+        
+        ret += (sum % 10) + '0';
+        
+        sum = sum / 10;
+    }
+    reverse(ret.begin(), ret.end());
+    cout << ret << '\n';
+
+    return 0;
+}
