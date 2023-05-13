7bd3f642f8737d7d8e4385c75336f24a4fb7e862
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-05-13 20:23:33 +0900

    [Gold III] Title: 순회강연, Time: 4 ms, Memory: 2292 KB -BaekjoonHub

---
+++ 백준/Gold/2109. 순회강연/순회강연.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, p, d, ret, max_d;
+vector<pair<int,int>> v;
+priority_queue<int, vector<int>, greater<int>> pq;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL); 
+    cin >> n;
+    if(n==0){
+        cout << 0 <<'\n'; return 0;
+    }
+    for(int i=0; i<n; i++){
+        cin >> p >> d;
+        v.push_back({d, p});
+    }
+    sort(v.begin(), v.end());
+    for(int i=0; i<n; i++){
+        pq.push(v[i].second);
+        if(pq.size() > v[i].first){
+            pq.pop();
+        }
+    }
+    while(pq.size()) {
+        ret += pq.top();
+        pq.pop();
+    }
+    
+    cout << ret << '\n';
+    return 0;
+}
