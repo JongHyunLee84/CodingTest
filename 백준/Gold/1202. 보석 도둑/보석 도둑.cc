0368f4e889008034c08e6f6005cc487fd4e72cf6
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-05-13 16:39:58 +0900

    [Gold II] Title: 보석 도둑, Time: 164 ms, Memory: 12256 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.cc
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -1,35 +1,28 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int n, k, idx = 0;
-vector<pair<int,int>> jew;
-vector<int> bag;
-int a, b;
-long long ret;
-int main()
-{
-    cin >> n >> k;
-    for(int i=0; i<n; i++){
-        cin >> a >> b;
-        jew.push_back({a,b});
-    }
-    for(int i=0; i<k; i++){
-        cin >> a;
-        bag.push_back(a);
-    }
-    sort(jew.begin(), jew.end());
-    sort(bag.begin(), bag.end());
-    priority_queue<int> pq;
-    for(int i : bag){
-        while(idx < n && i >= jew[idx].first){
-            pq.push(jew[idx].second);
-            idx++;
-        }
-        if(pq.size()){
-            ret += pq.top();
-            pq.pop();
-        }
-    }
-    cout << ret << '\n';
-    return 0;
-}
+#include<bits/stdc++.h>
+using namespace std; 
+typedef long long ll;
+ll n, k, ret, temp1, temp;
+int main(){
+	ios_base::sync_with_stdio(false);cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> n >> k; 
+	vector<pair<ll,ll>> v(n);
+	vector<ll> vv(k); 
+	for(int i = 0; i < n; i++){
+		cin >> v[i].first >> v[i].second; 
+	}
+	for(int i = 0; i < k; i++) cin >> vv[i]; 
+	sort(v.begin(), v.end());
+	sort(vv.begin(), vv.end());
+	priority_queue<ll> pq; 
+	
+	int j = 0; 
+	for(int i = 0; i < k; i++){
+		while(j < n && v[j].first <= vv[i]) pq.push(v[j++].second);
+		if(pq.size()){
+			ret += pq.top(); pq.pop();
+		}
+	} 
+	cout << ret << "\n"; 
+	return 0;
+}     
