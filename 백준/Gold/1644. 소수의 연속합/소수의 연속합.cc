b2ef5bfa489a550380baf069dda38ab505e18892
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-05-15 15:42:55 +0900

    [Gold III] Title: 소수의 연속합, Time: 36 ms, Memory: 9012 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
@@ -15,15 +15,14 @@ int main()
     }
     for(int i=2; i<= n; i++) if(check[i] == 0)v.push_back(i);
     // for(int i : v) cout << i << ' ';
-    int temp = 0;
-    int idx = 0;
-    int i = 0;
-    while(i < v.size()) {
-        temp += v[i];
-        // cout << temp << "and" << n << '\n';
-        if(temp == n)cnt++, temp = 0, i = ++idx;
-        else if(temp > n)i = ++idx, temp=0;
-        else i++;
+    int sum = 0;
+    int hi = 0;
+    int lo = 0;
+    while(true) {
+        if(sum >= n) sum -= v[lo++];
+        else if(hi == v.size())break;
+        else sum += v[hi++];
+        if(sum == n)cnt++;
     }
     
     cout << cnt << '\n';
