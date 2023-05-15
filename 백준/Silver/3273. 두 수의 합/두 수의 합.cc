8f7a9a194dc952ebb5fd60cae94fb198fa11bfb4
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-05-15 20:55:49 +0900

    [Silver III] Title: 두 수의 합, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -1,33 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+ll n, s, e, cnt, x;
 
-int n, x, ar[100004], ret;
 
 int main()
 {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL); 
+    cin.tie(NULL); cout.tie(NULL);
     cin >> n;
+    vector<int> v(n);
     for(int i=0; i<n; i++){
-        cin >> ar[i];
+        cin >> v[i];
     }
     cin >> x;
-    sort(ar, ar+n);
-    int a = 0;
-    int b = n-1;
-    
-    while(a < b){
-        if(ar[a] + ar[b] == x){
-            ret++;
-            a++;
-            b--;
-        } else if(ar[a] + ar[b] > x) {
-            b--;
-        } else {
-            a++;
-        }
+    e = n-1;
+    sort(v.begin(), v.end());
+    while(s<e){
+        if(v[s] + v[e] > x) e--;
+        else if (v[s] + v[e] == x)cnt++, e--;
+        else s++;
     }
-    cout << ret << '\n';
+    cout << cnt << '\n';
     return 0;
 }
