e6d4d2ed85cd2400fd3cbf58d964be29a13b91d0
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-05-16 21:19:23 +0900

    [Silver II] Title: 스타트와 링크, Time: 140 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
@@ -1,39 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int INF = 987654321;
-int n, ret = INF, ar[24][24], visited[24];
+int n, ret = INF, ar[24][24];
 
-int go(vector<int> a, vector<int> b){
+void go(int num){
+    vector<int> start, link;
     int asum = 0;
     int bsum = 0;
+    for(int i=0; i<n; i++){
+        if(num & (1<<i))start.push_back(i);
+        else link.push_back(i);
+    }
     for(int i=0; i<n/2; i++){
         for(int j=0; j<n/2; j++){
             if(i==j)continue;
-            asum += ar[a[i]][a[j]];
-            bsum += ar[b[i]][b[j]];
+            asum += ar[start[i]][start[j]];
+            bsum += ar[link[i]][link[j]];
         }
     }
-    return abs(asum - bsum);
-}
-
-void combi(int start, vector<int> temp_v) {
-    if(temp_v.size() == n/2) {
-        vector<int> start, link;
-        for(int i=0; i<n; i++){
-            if(visited[i])start.push_back(i);
-            else link.push_back(i);
-        }
-        ret = min(ret, go(start, link));
-        return;
-    }
-    for(int i=start+1; i<n; i++){
-        visited[i] = 1;
-        temp_v.push_back(i);
-        combi(i, temp_v);
-        visited[i] = 0;
-        temp_v.pop_back();
-    }
-    return;
+    ret = min(ret, abs(asum - bsum));
 }
 
 int main() {
@@ -43,8 +28,13 @@ int main() {
             cin >> ar[i][j];
         }
     }
-    vector<int> tv;
-    combi(-1, tv);
+    for(int i=0; i < (1<<n); i++){
+        int cnt = 0;
+        for(int j=0; j < n; j++){
+            if(i & (1<<j))cnt++;
+        }
+        if(cnt == n/2)go(i);
+    }
     cout << ret << '\n';
     return 0;
 }
