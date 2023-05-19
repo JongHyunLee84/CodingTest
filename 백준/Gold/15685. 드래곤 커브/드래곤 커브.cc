0847d7522a5da774139c487c036fdf10e6f73b97
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-05-19 23:16:54 +0900

    [Gold IV] Title: 드래곤 커브, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

--- 백준/Gold/15685. 드래곤 커브/드래곤 커브.cc
+++ 백준/Gold/15685. 드래곤 커브/드래곤 커브.cc
@@ -2,38 +2,41 @@
 using namespace std;
 int n, ar[104][104], ret;
 int x, y, d, g;
+vector<int> dragon[4][11];
+int dy[4] = {0,-1,0,1}, dx[4] = {1,0,-1,0};
+void make_dragon() {
+    for(int i=0; i<4; i++){
+        dragon[i][0].push_back(i);
+        dragon[i][1].push_back((i+1)%4);
+        for(int j=2; j<=10; j++){
+            int n = dragon[i][j-1].size();
+            for(int k=n-1; k>=0; k--){
+                dragon[i][j].push_back((dragon[i][j-1][k]+1)%4);
+            }
+            for(int k=0; k<n; k++){
+                dragon[i][j].push_back((dragon[i][j-1][k]));
+            }
+        }
+    }
+}
 
-void next(int x, int y, int d, vector<pair<int,int>> &v) {
-    if(d==0)v.push_back({y,x+1});
-    else if(d==1)v.push_back({y-1,x});
-    else if(d==2)v.push_back({y,x-1});
-    else v.push_back({y+1,x});
+void go(int x, int y, int d, int g){
+    ar[y][x] = 1;
+    for(int i=0; i<=g; i++){
+        for(int j : dragon[d][i]){
+            y += dy[j];
+            x += dx[j];
+            ar[y][x] = 1;
+        }
+    }
 }
 
 int main() {
     cin >> n;
+    make_dragon();
     for(int i=0; i<n; i++){
         cin >> x >> y >> d >> g;
-        vector<pair<int,int>> v;
-        v.push_back({y,x});
-        next(x, y, d, v);
-        int ny, nx ;
-        for(int j=0; j<g; j++){
-            for(int p=v.size()-1; p >0; p--){
-                ny = v[p].first - v[p-1].first;
-                nx = v[p].second - v[p-1].second;
-                if(ny == 0){
-                    if(nx < 0 )next(v[v.size()-1].second, v[v.size()-1].first, 3, v);//오른쪽
-                    else next(v[v.size()-1].second, v[v.size()-1].first, 1, v);//왼쪽
-                }
-                else {
-                    if(ny < 0)next(v[v.size()-1].second, v[v.size()-1].first, 2, v); // up
-                    else next(v[v.size()-1].second, v[v.size()-1].first, 0, v);//down
-                }
-            }
-        }
-        
-        for(auto j : v) ar[j.first][j.second] = 1;
+        go(x,y,d,g);
     }
     for(int i=0; i<100; i++){
         for(int j=0; j<100; j++){
