fb39ad9a21b9a203d06f45910166e677a06fcdfc
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-05-25 15:52:20 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 28 ms, Memory: 2296 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.cc
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.cc
@@ -11,37 +11,20 @@ int main() {
     for(int i=0; i<t; i++){
         cin >> n >> m;
         ll ret = 0;
-        vector<int> a,b;
+        vector<int> a(n),b(m);
         for(int i=0; i<n; i++){
-            cin >> temp;
-            a.push_back(temp);
+            cin >> a[i];
         }
         for(int i=0; i<m; i++){
-            cin >> temp;
-            b.push_back(temp);
+            cin >> b[i];
         }
         sort(b.begin(), b.end());
         
-        // for(ll i : b)cout << i << ' ';
-        // cout << '\n';
-        
         for(int i=0; i<n; i++){
             int lower = lower_bound(b.begin(), b.end(), a[i]) - b.begin();
-            int upper = upper_bound(b.begin(),b.end(), a[i]) - b.begin();
-            // cout << a[i] << " lower" << lower << " upper" << upper << '\n';
-            if(lower == upper){
-                if(b[lower-1] < a[i]){
-                    ret += lower;
-                }
-            } else {
-                if(b[lower-1] < a[i]){
-                    ret += lower;
-                }
-            }
-            // cout << ret << '\n';
+            ret += lower;
         }
-        v.push_back(ret);
+        cout << ret << '\n';
     }
-    for(ll i : v) cout<< i << '\n';
     return 0;
 }
