c3c2cbdafc47f2daf854264a04860023e9cc56d0
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-05-25 12:39:50 +0900

    [Silver I] Title: 기타 레슨, Time: 36 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.cc
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.cc
@@ -2,25 +2,19 @@
 using namespace std;
 typedef long long ll;
 const int INF = 987654321;
-ll n, m, ar[100003], ret = INF;
+ll n, m, ar[100003], ret = INF, sum, mx;
 
 bool check(int mid) {
-    int minus = 0, cnt = 0;
-    for(int i=1; i<n; i++){
-        // cout << ar[i] - minus << "==" << mid << "or" << ar[i+1]-minus << '>' << mid << '\n';
-        if( (ar[i]-minus) > mid)return false;
-        else if( (ar[i]-minus) == mid ){
-            minus = ar[i];
-            cnt++;
-        } else if( (ar[i+1]-minus) > mid) {
-            minus = ar[i];
-            cnt++;
-        } else if( (ar[i+1]-minus) == mid){
-            minus = ar[i+1];
+    if(mid < mx)return false;
+    int temp = mid, cnt = 0;
+    for(int i=1; i<=n; i++){
+        if(temp - ar[i] < 0){
+            temp = mid;
             cnt++;
         }
+        temp -= ar[i];
     }
-    if(minus != ar[n])cnt++;
+    if(temp != mid)cnt++;
     // cout << m << ">=" << cnt << '\n';
     return m >= cnt;
     
@@ -30,9 +24,10 @@ int main()
     cin >> n >> m;
     for(int i=1; i<=n; i++){
         cin >> ar[i];
-        ar[i] += ar[i-1];
+        mx = max(mx, ar[i]);
+        sum += ar[i];
     }
-    ll l = 1, r = ar[n], mid;
+    ll l = 1, r = sum, mid;
     while(l <= r) {
         mid = (l+r)/2;
         if(check(mid)){
