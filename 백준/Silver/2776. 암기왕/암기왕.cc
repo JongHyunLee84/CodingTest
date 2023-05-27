fd5d96ed506372d097864ad3accfc65c0ef784da
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-05-27 12:27:33 +0900

    [Silver IV] Title: 암기왕, Time: 644 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.cc
+++ 백준/Silver/2776. 암기왕/암기왕.cc
@@ -3,37 +3,32 @@ using namespace std;
 
 int t, n, m, temp;
 
-int check(int temp, vector<int> &v){
-    int l = 0, r = v.size() - 1;
-    int mid;
-    
-    while(l <= r) {
-        mid = (l+r)/2;
-        if(v[mid] > temp)r = mid - 1;
-        else if(v[mid] == temp)return 1;
-        else l = mid + 1;
-    }
-    return 0;
-}
-
 int main()
 {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL), cout.tie(NULL);
-    cin >> t; 
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> t;
     while(t--){
-        cin>>n;
-        vector<int> v;
-        while(n--){
+        cin >> n;
+        vector<int> vn(n);
+        for(int i=0; i<n; i++)cin >> vn[i];
+        sort(vn.begin(),vn.end());
+        cin >> m;
+        for(int i=0; i<m; i++){
             cin >> temp;
-            v.push_back(temp);
+            int l = 0, r = n-1, mid, ret = 0;
+            while(l <= r){
+                mid = (l+r)/2;
+                if(vn[mid] == temp){
+                    ret = 1;
+                    break;
+                }else if(vn[mid] > temp)r = mid-1;
+                else l = mid + 1;
+            }
+            cout << ret << '\n';
         }
-        sort(v.begin(),v.end());
-        cin>>m;
-        while(m--){
-            cin >> temp;
-            cout << check(temp,v) << '\n';;
-        }
+
     }
+
     return 0;
 }
