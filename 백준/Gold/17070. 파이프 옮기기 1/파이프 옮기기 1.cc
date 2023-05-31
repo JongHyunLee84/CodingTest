04a2d27c86b4443b51e6178ed4a47fe38f0895e2
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-05-31 23:25:58 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -1,32 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, ar[16][16], ret;
+int n, dp[20][20][3] ,ar[16][16], ret;
 
-void go(pair<int,int> start, pair<int,int> end) {
-    // cout << start.first << '.' << start.second << ' ' << end.first << '.' << end.second << '\n';
-    if(end.first == n-1 && end.second == n-1){
-        ret ++;
-        return;
+bool check(int y, int x, int dir){
+    if(dir == 0 || dir == 2){
+        if(!ar[y][x])return true;
+    }else if(dir == 1){
+        if(!ar[y][x] && !ar[y-1][x] && !ar[y][x-1])return true;
     }
-    int dif_y = end.first - start.first;
-    int dif_x = end.second - start.second;
-    if(dif_y > 0 && dif_x > 0){
-        // 세로
-        if(end.first + 1 < n && !ar[end.first+1][end.second])go({start.first+1,start.second+1}, {end.first+1, end.second});
-        // 대각
-        if(end.second + 1 < n && end.first+1 < n && !ar[end.first][end.second+1] && !ar[end.first+1][end.second+1] && !ar[end.first+1][end.second])go({start.first+1, start.second+1},{end.first+1, end.second+1});
-        // 가로
-        if(end.second + 1 < n && !ar[end.first][end.second+1])go({start.first+1, start.second+1}, {end.first, end.second+1});
-    }else if(dif_y > 0) {
-        // 세로
-        if(end.first + 1 < n && !ar[end.first+1][end.second])go({start.first+1,start.second}, {end.first+1, end.second});
-        // 대각
-        if(end.second + 1 < n && end.first+1 < n && !ar[end.first][end.second+1] && !ar[end.first+1][end.second+1] && !ar[end.first+1][end.second])go({start.first+1, start.second},{end.first+1, end.second+1});
-    }else {
-        if(end.second + 1 < n && !ar[end.first][end.second+1])go({start.first, start.second+1}, {end.first, end.second+1});
-        if(end.second + 1 < n && end.first+1 < n && !ar[end.first][end.second+1] && !ar[end.first+1][end.second+1] && !ar[end.first+1][end.second])go({start.first, start.second+1},{end.first+1, end.second+1});
-    }
+    return false;
 }
 
 int main() {
@@ -36,7 +19,23 @@ int main() {
             cin >> ar[i][j];
         }
     }
-    go({0,0}, {0,1});
+    dp[0][1][0] = 1;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(check(i, j+1, 0))dp[i][j+1][0] += dp[i][j][0];
+            if(check(i+1, j+1, 1))dp[i+1][j+1][1] += dp[i][j][0];
+            
+            if(check(i+1, j, 2))dp[i+1][j][2] += dp[i][j][2];
+            if(check(i+1, j+1, 1))dp[i+1][j+1][1] += dp[i][j][2];
+            
+            if(check(i, j+1, 0))dp[i][j+1][0] += dp[i][j][1];
+            if(check(i+1, j+1, 1))dp[i+1][j+1][1] += dp[i][j][1];
+            if(check(i+1, j, 2))dp[i+1][j][2] += dp[i][j][1];
+        }
+    }
+    ret = dp[n-1][n-1][0];
+    ret += dp[n-1][n-1][1];
+    ret += dp[n-1][n-1][2];
     cout << ret << '\n';
     return 0;
 }
