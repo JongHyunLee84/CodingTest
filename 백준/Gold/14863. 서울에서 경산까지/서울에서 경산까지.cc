b45b547f710d56d98f391272be6a824e69f7b376
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-06-23 17:18:30 +0900

    [Gold IV] Title: 서울에서 경산까지, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14863. 서울에서 경산까지/서울에서 경산까지.cc
+++ 백준/Gold/14863. 서울에서 경산까지/서울에서 경산까지.cc
@@ -1,17 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, K, lt, lm, ct, cm, dp[104][1000000];
+int N, K, lt, lm, ct, cm, ret;
 pair<int,int> load[104], cycle[104];
-
-int go(int idx, int sumT, int sumM) {
-    if(sumT > K)return -1e9;
-    if(idx == N)return sumM;
-    int &ret = dp[idx][sumM];
-    if(ret != -1)return ret;
-    ret = 0;
-    ret = max(go(idx+1, sumT + load[idx+1].first, sumM + load[idx+1].second), go(idx+1, sumT + cycle[idx+1].first, sumM + cycle[idx+1].second));
-    return ret;
+vector<pair<int,int>> v, v2;
+void go(int idx,int end ,int sumT, int sumM, vector<pair<int,int>> &v) {
+    if(sumT > K)return;
+    if(idx == end){
+        v.push_back({sumM,sumT});
+        return;
+    }
+    go(idx+1, end, sumT + load[idx].first, sumM + load[idx].second, v);
+    go(idx+1, end, sumT + cycle[idx].first, sumM + cycle[idx].second, v);
 }
 
 int main()
@@ -22,7 +22,27 @@ int main()
         load[i] = {lt,lm};
         cycle[i] = {ct,cm};
     }
-    memset(dp, -1, sizeof(dp));
-    cout << max(go(0, load[0].first, load[0].second), go(0, cycle[0].first, cycle[0].second)) << '\n';
+    go(0, N/2, 0, 0, v);
+    go(N/2, N, 0, 0, v2);
+    // int endPoint = N % 2 != 0 ? N / 2 + 1 : N / 2;
+    // for(int i=0; i<N/2; i++){
+    //     cout << v[i].first << ' ' << v[i].second;
+        
+    // }
+    // cout << '\n';
+    // for(int i=0; i<endPoint; i++) {
+    //     cout << v2[i].first << ' ' << v2[i].second << ' ';
+    // }
+    // cout << "end" << '\n';
+    for(int i=0; i<v.size(); i++){
+        for(int j=0; j<v2.size(); j++){
+            if( (v[i].second + v2[j].second) <= K ){
+                
+            ret = max(ret, v[i].first + v2[j].first);
+        }
+        }
+
+    }
+    cout << ret << '\n';
     return 0;
 }
