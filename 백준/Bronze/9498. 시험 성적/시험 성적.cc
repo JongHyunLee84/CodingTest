2633d6dfe22550ffb1d41a8c6332fd821b9a7f9c
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-07-08 00:05:53 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long a, b;
+int main()
+{
+    cin >> a;
+    if(a >= 90)cout << 'A';
+    else if(a >= 80)cout << 'B';
+    else if(a >= 70)cout << 'C';
+    else if(a >= 60)cout << 'D';
+    else cout << 'F';
+    return 0;
+}
