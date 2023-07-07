89a196f3f49eca5fe3fd68d69a14119d9871cec4
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-07-07 23:56:27 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long a, b, c;
+int main()
+{
+    
+    cout << "\\    /\\" << '\n' <<  " )  ( ')" << '\n' << "(  /  )" << '\n' << " \\(__)|";
+
+    return 0;
+}
