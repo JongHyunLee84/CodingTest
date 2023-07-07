0af784123618eaed46cbb47083f65bb6feefd7bd
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-07-07 23:59:30 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long a, b, c;
+int main()
+{
+    
+    cout << "|\\_/|" << '\n' <<  "|q p|   /}" << '\n' << "( 0 )\"\"\"\\" << '\n' << "|\"^\"`    |" << '\n' << "||_/=\\\\__|";
+
+    return 0;
+}
