83849ab857191570b73e7205ed1ad5a60135c495
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-07-08 14:16:29 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a, b, c;
+
+int main()
+{
+    cin >> a;
+    for(int i=1; i<=a; i++){
+        b += i;
+    }
+    cout << b << '\n';
+    return 0;
+}
