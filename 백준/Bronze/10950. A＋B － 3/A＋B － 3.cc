f6ae8deb0908cd1681745c1dc42db20afc717ca6
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-07-08 14:15:26 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a, b, c;
+
+int main()
+{
+    cin >> a;
+    for(int i=0; i<a; i++){
+        cin >> b >> c;
+        cout << b + c << '\n';
+    }
+
+    return 0;
+}
