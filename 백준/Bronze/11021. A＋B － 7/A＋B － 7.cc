a104b72825ae3ab0dc08901ddca44a2278bb653a
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-07-09 22:25:19 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a, b, c;
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+    cin >> a;
+    for(int i=1; i<=a; i++){
+        cin >> b >> c;
+        cout << "Case #" << i << ": " << b + c << '\n';
+    }
+
+    return 0;
+}
