7c8f5aebccdd9a54e27e04c3eb2de750478f539e
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-07-09 22:28:08 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a, b, c;
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+    cin >> a;
+    for(int i=1; i<=a; i++){
+        for(int j=1; j<=i; j++){
+            cout << "*";
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
