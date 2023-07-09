6138abad34c93ca99d146015df2604d847a7b2a5
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-07-09 22:38:26 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a, b, c;
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+    while(cin >> a >> b){
+        cout << a + b << '\n';
+    }
+
+    return 0;
+}
