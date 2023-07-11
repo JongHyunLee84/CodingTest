9319b9d556bcad152f53a0ee2930a9f11e5c0880
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-07-11 22:02:38 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int n;
+string s;
+char c;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> c;
+    cout << c - NULL << '\n';
+    
+    return 0;
+}
