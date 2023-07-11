8fc2d1cfa4b33c738efc683bee4fb247681a419c
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-07-11 23:23:29 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, ret;
+string s, a, b;
+char c;
+map<char, int> mp;
+int main()
+{
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(NULL); cout.tie(NULL);
+    while(1){
+        getline(cin, s);
+        if(s == "")break;
+        cout << s << '\n';
+    }
+    
+    return 0;
+}
