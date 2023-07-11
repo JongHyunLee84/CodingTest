60148e488423c982973220b3e852d80bdab8767f
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-07-11 23:27:14 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, ret;
+string s, a, b;
+char c;
+map<char, int> mp;
+int main()
+{
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(NULL); cout.tie(NULL);
+    cout<< "         ,r'\"7" << '\n';
+    cout<< "r`-_   ,'  ,/" << '\n';
+    cout<< " \\. \". L_r'" << '\n';
+    cout<< "   `~\\/" << '\n';
+    cout<< "      |" << '\n';
+    cout<< "      |" << '\n';
+    return 0;
+}
