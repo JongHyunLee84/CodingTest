dee717bfcde1caeaf3195f11d151b0236e1c1f49
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-07-26 22:37:43 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL); cout.tie(NULL);
+    cout << 1 << '\n' << 0 << '\n';
+    
+
+    return 0;
+}
