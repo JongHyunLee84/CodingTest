56668144db0a75c6aa17b186de3341837adf460f
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-08-04 00:32:39 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 3020 ms, Memory: 2552 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -1,18 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, k, temp, ar[10004];
+int n, temp;
 map<int, int> mp;
+
 int main()
 {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> n;
+    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+    
+    cin >> n; 
     for(int i=0; i<n; i++){
         cin >> temp;
         mp[temp]++;
     }
+    
     for(int i=1; i<=10000; i++){
-        for(int j=0; j<mp[i]; j++)cout << i << '\n';
+        for(int j=0; j<mp[i]; j++){
+            cout << i << '\n';
+        }
     }
+
     return 0;
 }
