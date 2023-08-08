c6afb881f02fffde913afd602f5c41c5c5ae1c92
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-08-08 14:03:47 +0900

    [Gold II] Title: 성냥개비, Time: 4 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/3687. 성냥개비/성냥개비.cc
+++ 백준/Gold/3687. 성냥개비/성냥개비.cc
@@ -1,42 +1,41 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int N, sticks, ar[10] = {6, 2, 5, 5, 4, 5, 6, 3, 7, 6 };
-string dp[104], max_str = "111111111111111111111111111111111111111111111111119";
-
+#include<bits/stdc++.h>
+//2. 탑버텀 
+using namespace std;  
+int a[10] = {6, 2, 5, 5, 4, 5, 6, 3, 7, 6},t, n;
+string dp[104], dp2[104], MAX_STR = "11111111111111111111111111111111111111111111111111111";
 string get_min_str(string a, string b){
-    if(a.size() == b.size()) return (a > b ? b : a);
-    if(a.size() < b.size())return a;
-    else return b;
+	if(a.size() == b.size()) return (a < b ? a : b); 
+	if(a.size() < b.size()) return a; 
+	else return b;  
 }
-
-string find_max(int s) {
-    string ret = "";
-    if(s & 1){
-        s -= 3;
-        ret += "7";
-    }
-    while(s){
-        ret += "1";
-        s -= 2;
-    }
-    return ret;
+string findMax(int here){  
+	string ret = "";
+	if(here & 1){ret += '7'; here -= 3; }
+	while(here){
+		ret += '1';
+		here -= 2; 
+	}
+	return ret;   
 }
-
-int main() {
-    cin>>N;
-    fill(dp, dp + 104, max_str);
-    dp[0] = "";
-    for(int i=2; i<=100; i++){
-        for(int j=0; j<10; j++){
-            if(i-ar[j] < 0)continue;
-            if(j == 0 && dp[i-ar[j]] == "")continue;
-            dp[i] = get_min_str(dp[i], dp[i-ar[j]] + to_string(j));
-        }
-    }
-    for(int i=0; i<N; i++){
-        cin >> sticks;
-        cout << dp[sticks] << ' ' << find_max(sticks) << '\n';
-    }
-    return 0;
+string findMin(int here){  
+	if(here == 0) return ""; 
+	string &ret = dp[here];  
+	if(ret != MAX_STR) return ret;  
+	for(int i = 0; i <= 9; i++){ 
+		if(here - a[i] < 0) continue; 
+		if(here == n && i == 0) continue; 
+		ret = get_min_str(ret, to_string(i) + findMin(here - a[i]));
+	} 
+	return ret; 
+} 
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> t;     
+	while(t--){   
+		cin >> n;  
+		fill(dp, dp + 104, MAX_STR);   
+		cout << findMin(n) << " " << findMax(n) << "\n";
+	}
 }
