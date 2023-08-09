f6af103f3db5d10674f41c023158bc13a7a26fbd
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-08-09 15:57:55 +0900

    [Gold III] Title: 종이 조각, Time: 36 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14391. 종이 조각/종이 조각.cc
+++ 백준/Gold/14391. 종이 조각/종이 조각.cc
@@ -1,52 +1,48 @@
 #include <bits/stdc++.h>
-using namespace std;   
+using namespace std;
 
-int n, m, ar[5][5], ret;
-string temp;
+int n, m, ar[6][6], visited[6][6], result;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
+int main()
+{
+    // ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     cin >> n >> m;
     for(int i=0; i<n; i++){
-        cin >> temp;
+        string s;
+        cin >> s;
         for(int j=0; j<m; j++){
-            ar[i][j] = temp[j] - '0';
+            ar[i][j] = s[j] - '0';
         }
     }
-    
-    for(int s=0; s<(1<<(n*m)); s++){
-        int sum = 0;
+    for(int s=0; s<(1<<n*m); s++){
+        int ret = 0;
         for(int i=0; i<n; i++){
-            int cur = 0;
+            string str;
             for(int j=0; j<m; j++){
-                int k = i * m + j;
-                if( (s & (1<<k)) == 0 ){
-                    cur = cur * 10 + ar[i][j];
-                } else {
-                    sum += cur;
-                    cur = 0;
-                }
+                if(s & (1 << i*m +j)){
+                    str += ar[i][j] + '0';
+                }else{
+                    if(str!="")ret += stoi(str);
+                    str = "";
+                } 
             }
-            sum += cur;
+            if(str!="")ret += stoi(str);
         }
-        
-        for (int j = 0; j < m; j++) {
-            int cur = 0;
-            for (int i = 0; i < n; i++) {
-                int k = i * m + j;
-                if ((s & (1<<k)) != 0) {
-                    cur = cur * 10 + ar[i][j];
-                } else {
-                    sum += cur;
-                    cur = 0;
-                }
+        for(int j=0; j<m; j++){
+            string str;
+            for(int i=0; i<n; i++){
+                if(!(s & (1 << i*m +j))){
+                    str += ar[i][j] + '0';
+                }else{
+                    if(str!="")ret += stoi(str);
+                    str = "";
+                } 
             }
-            sum += cur;
+            if(str!="")ret += stoi(str);
         }
-        ret = max(ret, sum);
+        // cout << s << ' ' << ret << '\n';
+        result = max(result, ret);
     }
-    
-    cout << ret << '\n';
+    cout << result << '\n';
     return 0;
 }
