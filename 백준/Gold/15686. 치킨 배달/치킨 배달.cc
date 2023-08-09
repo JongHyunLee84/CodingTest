c8b3035eb462e50cae301e8b3707f3974b70470e
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-08-09 12:07:27 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,68 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m, temp, result = 99999999;
-vector<pair<int, int>> chicken, home;
-vector<vector<int>> chicken_combi;
-vector<int> test;
-void combi(int a, vector<int> v)
-{
-    if (v.size() == m)
-    {
-        chicken_combi.push_back(v);
+int n, m, temp, result = 1e9;
+vector<pair<int,int>> houses, chicken;
+vector<vector<int>> com;
+
+void combi(int idx, vector<int> v){
+    if(v.size() == m) {
+        com.push_back(v);
         return;
     }
-    for (int i = a + 1; i < chicken.size(); i++)
-    {
+    for(int i = idx+1; i < chicken.size(); i++){
         v.push_back(i);
         combi(i, v);
         v.pop_back();
     }
-    return;
 }
 
 int main()
 {
     cin >> n >> m;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
             cin >> temp;
-            test.push_back(temp);
-            if (temp == 1)
-            {
-                home.push_back({i, j});
-            }
-            else if (temp == 2)
-            {
-                
-                chicken.push_back({i, j});
-            }
+            if(temp == 1)houses.push_back({i,j});
+            else if(temp == 2)chicken.push_back({i,j});
         }
     }
-    // for(int i : test) cout << i << ' ';
-    // for (auto i : chicken)
-    //     cout << i.first << i.second << '\n';
     vector<int> v;
     combi(-1, v);
-
-    for (auto clist : chicken_combi)
-    {
+    // for(int i=0; i<com.size(); i++){
+    //     for(int j=0; j<com[i].size(); j++){
+    //         cout << com[i][j] << ' ';
+    //     }
+    //     cout << '\n';
+    // }
+    for(int i=0; i<com.size(); i++){
         int ret = 0;
-        for (auto h : home)
-        {
-            int max_min = 987654321;
-            for (int c : clist)
-            {
-                int lo = abs(h.first - chicken[c].first) + abs(h.second - chicken[c].second);
-                max_min = min(lo, max_min);
+        for(int j=0; j<houses.size(); j++){
+            int dis = 1e9;
+            for(int k=0; k<m; k++){
+                dis = min(dis, (abs(houses[j].first - chicken[com[i][k]].first) + abs(houses[j].second - chicken[com[i][k]].second)));
+                // cout << dis << '\n';
             }
-            ret += max_min;
+            ret += dis;
         }
         result = min(ret, result);
     }
-
     cout << result << '\n';
     return 0;
 }
