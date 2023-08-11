c009a2dd9c1bce647320972133e6c3b61f64d605
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-08-11 16:25:47 +0900

    [Silver II] Title: 스타트와 링크, Time: 156 ms, Memory: 17324 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
@@ -1,39 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int INF = 987654321;
-int n, ret = INF, ar[24][24];
 
-void go(int num){
-    vector<int> start, link;
-    int asum = 0;
-    int bsum = 0;
-    for(int i=0; i<n; i++){
-        if(num & (1<<i))start.push_back(i);
-        else link.push_back(i);
+int n, ar[24][24], ret = 1e9;
+vector<vector<int>> v;
+
+void combi(int idx, vector<int> vv){
+    if(vv.size() == n/2){
+        v.push_back(vv);
+        // for(int i:vv)cout << i << ' ';
+        // cout << '\n';
+        return;
     }
-    for(int i=0; i<n/2; i++){
-        for(int j=0; j<n/2; j++){
-            if(i==j)continue;
-            asum += ar[start[i]][start[j]];
-            bsum += ar[link[i]][link[j]];
-        }
+    for(int i=idx+1; i<n; i++){
+        vv.push_back(i);
+        combi(i, vv);
+        vv.pop_back();
     }
-    ret = min(ret, abs(asum - bsum));
 }
 
-int main() {
+void compare(vector<int> vv){
+    int sum1= 0, sum2 = 0;
+    vector<int> against;
+    for(int i=0; i<n; i++){
+        if(find(vv.begin(), vv.end(), i) == vv.end())against.push_back(i);
+    }
+    
+    // for(int i : vv)cout << i << ' ';
+    // cout << '\n';
+
+    for(int i=0; i<vv.size(); i++){
+        for(int j=0; j<vv.size(); j++){
+            if(i == j)continue;
+            sum1 += ar[vv[i]][vv[j]];
+        }
+    }
+    
+    // cout << "sum1 : " << sum1 << '\n';
+    
+    // for(int i : against)cout << i << ' ';
+    // cout << '\n';
+    for(int i=0; i<vv.size(); i++){
+        for(int j=0; j<vv.size(); j++){
+            if(i == j)continue;
+            sum2 += ar[against[i]][against[j]];
+        }
+    }
+    // cout << "sum2 : " << sum2 << '\n';
+    ret = min(ret, abs(sum2-sum1));
+}
+
+int main()
+{
     cin >> n;
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
             cin >> ar[i][j];
         }
     }
-    for(int i=0; i < (1<<n); i++){
-        int cnt = 0;
-        for(int j=0; j < n; j++){
-            if(i & (1<<j))cnt++;
-        }
-        if(cnt == n/2)go(i);
+    vector<int> vv;
+    combi(-1, vv);
+    for(int i=0; i<v.size(); i++){
+        compare(v[i]);
     }
     cout << ret << '\n';
     return 0;
