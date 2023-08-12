1e5fa5b8db86f068ab6d5549aec6bf6c2a19d57f
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-08-12 13:40:00 +0900

    [Gold IV] Title: 뱀, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.cc
+++ 백준/Gold/3190. 뱀/뱀.cc
@@ -1,61 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, ar[104][104], k, l, cnt, direction = 0;
-queue<pair<int,char>> turn;
-list<pair<int,int>> v;
-int a, b, dy[4] = {0,1,0,-1}, dx[4] = {1,0,-1,0};
-char c;
 
-bool move(){
-    int ny = v.front().first + dy[direction];
-    int nx = v.front().second + dx[direction];
-    if(ny < 0 || nx < 0 || ny >= n || nx >= n || ar[ny][nx] == -1)return false;
-    if(ar[ny][nx] == 1)v.push_front({ny,nx});
-    else {
-        v.push_front({ny,nx});
-        auto temp = v.back();
-        ar[temp.first][temp.second] = 0;
-        v.pop_back();
+int n, ar[104][104], k, l, a, b, ret, cnt, dy[4] = {0, 1, 0, -1}, dx[4] = {1, 0, -1, 0}, dir;
+char c;
+vector<pair<int, char>> v;
+list<pair<int,int>> li;
+bool move() {
+    int ny = dy[dir] + li.back().first;
+    int nx = dx[dir] + li.back().second;
+    if(ny < 0 || nx < 0 || ny >= n || nx >= n)return false;
+    if(ar[ny][nx] == 2)return false;
+    if(!ar[ny][nx]){
+        ar[li.front().first][li.front().second] = 0;
+        li.pop_front();
     }
-    ar[ny][nx] = -1;
+    ar[ny][nx] = 2;
+    li.push_back({ny, nx});
     return true;
 }
 
-int main() {
+void rotate_(char ch){
+    if(ch == 'L')dir = (dir + 3) % 4;
+    else dir = (dir + 1) % 4;
+}
+
+int main()
+{
     cin >> n >> k;
     for(int i=0; i<k; i++){
-        cin >> a >> b;
-        ar[--a][--b] = 1;
+        cin >> a >> b; a--; b--;
+        ar[a][b] = 1;
     }
     cin >> l;
     for(int i=0; i<l; i++){
         cin >> a >> c;
-        turn.push({a,c});
+        v.push_back({a,c});
     }
-    ar[0][0] = -1;
-    v.push_back({0,0});
-    while(true) {
-        if(move())cnt++;
-        else break;
-        
-        // cout << '\n';
-        // for(int i=0; i<n; i++){
-        //     for(int j=0; j<n; j++){
-        //         cout << ar[i][j] << ' ';
-        //     }
-            
-        //     cout << '\n';
-        // }
-        // cout << "cnt : " << cnt << '\n';
-        
-        if(turn.size() && cnt >= turn.front().first){
-            auto temp = turn.front();
-            turn.pop();
-            if(temp.second == 'L')direction = (direction + 3)%4;
-            else direction = (direction+1)%4;
+    ar[0][0] = 2;
+    li.push_back({0,0});
+    while(1){
+        ret++;
+        if(!move())break;
+        if(ret == v[cnt].first){
+            rotate_(v[cnt].second);
+            cnt++;
         }
-        
     }
-    cout << cnt+1 << '\n';
+    cout << ret << '\n';
     return 0;
 }
