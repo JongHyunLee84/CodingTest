b49c4ca286416007d18c6ab4ada476b5b6b31a38
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-08-14 12:07:57 +0900

    [Silver III] Title: 큰 수 A+B (2), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/15353. 큰 수 A＋B （2）/큰 수 A＋B （2）.cc
+++ 백준/Silver/15353. 큰 수 A＋B （2）/큰 수 A＋B （2）.cc
@@ -1,28 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string a, b, ret;
+string a, b;
 
-int main(){
-    cin >> a >> b;
-    int sum = 0;
-    while(a.size() || b.size() || sum){
-        // cout << "here" << '\n';
-        if(a.size()){
-            sum += a.back() - '0';
-            a.pop_back();
-        }
-        if(b.size()){
-            sum += b.back() - '0';
-            b.pop_back();
-        }
-        
+string plus_(string a, string b) {
+    reverse(a.begin(),a.end()); reverse(b.begin(),b.end());
+    int idx = min(a.size(), b.size());
+    string bigger = "";
+    if(a.size() > b.size())bigger = a;
+    else bigger = b;
+    
+    string ret = "";
+    int p = 0;
+    for(int i=0; i<idx; i++){
+        int sum = (a[i] - '0') + (b[i] - '0') + p;
+        // cout << sum << '\n';
+        p = sum / 10;
         ret += (sum % 10) + '0';
-        
-        sum = sum / 10;
+        // cout << ret << '\n';
+        // cout << p << '\n';
     }
-    reverse(ret.begin(), ret.end());
-    cout << ret << '\n';
+    for(int i=idx; i<bigger.size(); i++){
+        int sum = (bigger[i] - '0' + p);
+        // cout << bigger[i] << '\n';
+        // cout << "p" << p << '\n';
+        // cout << sum << '\n';
+        p = sum / 10;
+        ret += (sum % 10) + '0';
+        // cout << "hree" << '\n';
+    }
+    if(p)ret += p + '0';
+    reverse(ret.begin(),ret.end());
+    return ret;
+}
 
+int main()
+{
+    cin >> a >> b;
+    cout << plus_(a,b) << '\n';
     return 0;
 }
