079e9dba322ef9aebc3aad237fa78ba0d28b9faf
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-08-14 11:06:02 +0900

    [Silver V] Title: 분수찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.cc
+++ 백준/Silver/1193. 분수찾기/분수찾기.cc
@@ -1,14 +1,30 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int i = 1, n;
+
+int cnt = 1, n, a, b, m;
+
 int main()
 {
-    cin >> n;
-    while(n > i){
-        n -= i;
-        i++;
+    cin >> m;
+    while(n < m){
+        n += cnt;
+        cnt++;
     }
-    if(i % 2 == 0)cout << n << '/' << i + 1 - n;
-    else cout << i + 1 - n << '/' << n;
+    if(cnt % 2 == 0){
+        a = cnt - 1;
+        b = 1;
+    }else {
+        a = 1;
+        b = cnt - 1;
+    }
+    // cout << n - 9 << '\n';
+    for(int i=0; i<n-m; i++){
+        if(cnt % 2 == 0){
+            a--; b++;
+        }else {
+            a++; b--;
+        }
+    }
+    cout << b << '/' << a << '\n';
     return 0;
 }
