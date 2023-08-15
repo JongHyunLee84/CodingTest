7dfb7bc49448e658fdd0254465fd96a1a83ae656
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-08-15 14:33:41 +0900

    [Gold III] Title: 현욱은 괄호왕이야!!, Time: 12 ms, Memory: 3844 KB -BaekjoonHub

--- 백준/Gold/15926. 현욱은 괄호왕이야！！/현욱은 괄호왕이야！！.cc
+++ 백준/Gold/15926. 현욱은 괄호왕이야！！/현욱은 괄호왕이야！！.cc
@@ -1,30 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, visited[200001], cnt, ret;
-stack<int> st;
+int n, ar[200004], ret, cnt;
 string s;
-int main(){
+stack<int> st;
+int main() {
     cin >> n >> s;
     for(int i=0; i<n; i++){
         if(s[i] == '(')st.push(i);
-        else {
-            if(st.size()){
-                visited[st.top()] =1;
-                visited[i] =1;
-                st.pop();
-            }
+        else if(st.size()){
+            ar[i] = ar[st.top()] = 1;
+            st.pop();
         }
     }
     for(int i=0; i<n; i++){
-        if(visited[i])cnt++;
-        else {
-            ret = max(ret, cnt);
+        if(ar[i]){
+            cnt++;
+        }else {
             cnt = 0;
         }
+        ret = max(ret, cnt);
     }
-    ret = max(ret,cnt);
-    cout << ret << '\n';
-    
+    cout << ret << "\n";
     return 0;
 }
+
