19e68dca80c74530b87eddf7cfeef478897b9a4c
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-08-15 21:43:56 +0900

    [Gold IV] Title: 드래곤 커브, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

--- 백준/Gold/15685. 드래곤 커브/드래곤 커브.cc
+++ 백준/Gold/15685. 드래곤 커브/드래곤 커브.cc
@@ -1,48 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, ar[104][104], ret;
-int x, y, d, g;
-vector<int> dragon[4][11];
-int dy[4] = {0,-1,0,1}, dx[4] = {1,0,-1,0};
-void make_dragon() {
-    for(int i=0; i<4; i++){
-        dragon[i][0].push_back(i);
-        dragon[i][1].push_back((i+1)%4);
-        for(int j=2; j<=10; j++){
-            int n = dragon[i][j-1].size();
-            for(int k=n-1; k>=0; k--){
-                dragon[i][j].push_back((dragon[i][j-1][k]+1)%4);
-            }
-            for(int k=0; k<n; k++){
-                dragon[i][j].push_back((dragon[i][j-1][k]));
-            }
-        }
-    }
-}
 
-void go(int x, int y, int d, int g){
-    ar[y][x] = 1;
-    for(int i=0; i<=g; i++){
-        for(int j : dragon[d][i]){
-            y += dy[j];
-            x += dx[j];
-            ar[y][x] = 1;
+int n, ar[104][104], ret, x, y, d, g, dy[4] = {0, -1, 0, 1}, dx[4] = {1, 0, -1, 0};
+struct Dragon {
+    int x, y, d;
+};
+
+Dragon dra[24];
+
+void print() {
+        for(int i=0; i<=8; i++){
+        for(int j=0; j<=8; j++){
+            cout << ar[i][j] << ' ';
         }
-    }
+        cout << '\n';
+        }
+        cout << '\n';
 }
 
 int main() {
+    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
     cin >> n;
-    make_dragon();
     for(int i=0; i<n; i++){
         cin >> x >> y >> d >> g;
-        go(x,y,d,g);
+        vector<int> v;
+        v.push_back(d);
+        ar[y][x] = 1;
+        int ny = y + dy[d];
+        int nx = x + dx[d];
+        ar[ny][nx] = 1;
+        pair<int,int> last_point = {ny,nx};
+        // print();
+        
+        for(int j=0; j<g; j++){
+            for(int idx=v.size()-1; idx >=0; idx--){
+                int dir = (v[idx]+1) % 4;
+                int ny = last_point.first + dy[dir];
+                int nx = last_point.second + dx[dir];
+                last_point = {ny, nx};
+                ar[ny][nx] = 1;
+                v.push_back(dir);
+                // print();
+            }
+        }
     }
     for(int i=0; i<100; i++){
         for(int j=0; j<100; j++){
-            if(ar[i][j] && ar[i][j+1] && ar[i+1][j] && ar[i+1][j+1])ret++;
+            int cnt = 0;
+            cnt += ar[i][j];
+            cnt += ar[i][j+1];
+            cnt += ar[i+1][j];
+            cnt += ar[i+1][j+1];
+            if(cnt == 4)ret++;
         }
     }
-    cout << ret <<'\n';
+    cout << ret << '\n';
     return 0;
 }
