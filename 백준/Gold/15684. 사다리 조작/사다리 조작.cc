c5a2775231907699d3f411a590a64a029d157dbd
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-08-23 00:22:08 +0900

    [Gold III] Title: 사다리 조작, Time: 972 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.cc
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.cc
@@ -1,45 +1,81 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int INF = 987654321;
-int n, m, h, visited[34][14], ret = INF;
-int a, b;
+
+int n, m, h, connected[34][14], a, b, ret = 4, cnt;
+
+void print(){
+    cout << '\n';
+    for(int i=0; i<h; i++){
+        for(int j=0; j<n; j++){
+            cout << connected[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+}
 
 bool check() {
-    for(int i=1; i<=n; i++){
-        int temp = i;
-        for(int j=1; j<=h; j++){
-            if(visited[j][temp])temp++;
-            else if(visited[j][temp-1])temp--;
+    for(int i=0; i<n; i++){
+        int idx = i;
+        for(int j=0; j<h; j++){
+            if(connected[j][idx]){
+                if(idx && connected[j][idx-1]==connected[j][idx])idx -= 1;
+                else idx += 1;
+            }
         }
-        if(temp != i) return false;
+        if(idx != i)return false;
     }
-return true;    
+    return true;
 }
 
-void go(int start, int cnt){
-    if(cnt > 3 || cnt >= ret) return;
+void go(int y, int x, int idx){
+    // if(idx==3 && connected[3][1] && connected[3][2] && connected[4][0] && connected[4][1])print();
+    if(idx > 3)return;
     if(check()){
-        ret = min(ret, cnt);
+        // 아래 잘 체크된거 있는데 ?
+        // if(idx == 3)cout << connected[3][1] << ' ' << connected[3][2] << ' ' << connected[4][0] << ' ' << connected[4][1] << ' ' << connected[4][2] << ' ' << connected[4][3] << '\n';
+        ret = min(ret, idx);
+        return;
     }
-    
-    for(int i=start; i<=h; i++){
-        for(int j=1; j<=n; j++){
-            if(visited[i][j] || visited[i][j-1] || visited[i][j+1])continue;
-            visited[i][j] = 1;
-            go(i,cnt+1);
-            visited[i][j] = 0;
+    for(int i=y; i<h; i++){
+        for(int j=0; j<n-1; j++){
+            if(connected[i][j] || connected[i][j+1])continue;
+            connected[i][j] = idx+2;
+            connected[i][j+1] = idx+2;
+            go(i, j, idx+1);
+            connected[i][j] = 0;
+            connected[i][j+1] = 0;
         }
     }
 }
-int main() {
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+
     cin >> n >> m >> h;
-    for(int i=0; i<m; i++){
-        cin >> a >> b;
-        visited[a][b] = 1;
+    if(m == 0){
+        cout << 0 << '\n';
+        return 0;
     }
-    
-    go(1,0);
-    if(ret == INF)cout << -1 << '\n';
+    int idx = m;
+    for(int i=0; i<m; i++){
+        cin >> a >> b; a--; b--;
+        connected[a][b] = idx+1;
+        connected[a][b+1] = idx+1;
+        idx++;
+    }
+    // for(int i=0; i<h; i++){
+    //     for(int j=0; j<n-1; j++){
+    //         if(connected[i][j] || connected[i][j+1])continue;
+    //         connected[i][j] = 1;
+    //         connected[i][j+1] = 1;
+    //         go(1);
+    //         connected[i][j] = 0;
+    //         connected[i][j] = 0;
+    //     }
+    // }
+    go(0, 0, 0);
+    if(ret > 3)cout << -1 << '\n';
     else cout << ret << '\n';
     return 0;
 }
