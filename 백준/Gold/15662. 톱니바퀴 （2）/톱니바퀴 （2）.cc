8d44d36db8981c13cbfd1fd9ea29c2589e070f00
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-08-24 01:02:12 +0900

    [Gold V] Title: 톱니바퀴 (2), Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/15662. 톱니바퀴 （2）/톱니바퀴 （2）.cc
+++ 백준/Gold/15662. 톱니바퀴 （2）/톱니바퀴 （2）.cc
@@ -1,50 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int t, k, ret, num, dir;
-vector<vector<int>> wheels;
+int T, K, a, b, ret, visited[1004];
 string s;
-void rotate_left(int n, int d){
-    if(n < 0)return;
-    if(n - 1 >= 0 && wheels[n][6] != wheels[n-1][2]){
-        rotate_left(n-1, d * -1);
-    }
-        if(d == 1)rotate(wheels[n].rbegin(), wheels[n].rbegin() + 1, wheels[n].rend());
-        else rotate(wheels[n].begin(), wheels[n].begin() + 1, wheels[n].end());
-}
-void rotate_right(int n, int d){
-    if(n >= t)return;
-    if(n + 1 < t && wheels[n][2] != wheels[n+1][6]){
-        rotate_right(n+1, d * -1);
-    }
-        if(d == 1)rotate(wheels[n].rbegin(), wheels[n].rbegin() + 1, wheels[n].rend());
-        else rotate(wheels[n].begin(), wheels[n].begin() + 1, wheels[n].end());
-}
+vector<string> v;
 
+void rotate_(int idx, int dir){
+        visited[idx] = 1;
+        if(idx-1 >= 0 && !visited[idx-1] && v[idx][6] != v[idx-1][2]){
+            rotate_(idx-1, dir * -1);
+        }
+        if(idx+1 < T && !visited[idx+1] && v[idx][2] != v[idx+1][6]){
+            rotate_(idx+1, dir * -1);
+        }
+
+    if(dir == 1)rotate(v[idx].rbegin(), v[idx].rbegin()+1, v[idx].rend());
+    else rotate(v[idx].begin(), v[idx].begin()+1, v[idx].end());
+    // cout << idx << ' ' << v[idx] << '\n';
+//     if(dir == 1)rotate(v[idx].begin(), v[idx].begin()+1, v[idx].end());
+//     else rotate(v[idx].rbegin(), v[idx].rbegin()+1, v[idx].rend());
+}
 
 int main() {
-    cin >> t;
-    for(int i=0; i<t; i++){
+ // 오른족 2, 왼쪽 6 인덱스
+    cin >> T;
+    
+    for(int i=0; i<T; i++){
         cin >> s;
-        vector<int> v;
-        for(char j : s)v.push_back(j-'0');
-        wheels.push_back(v);
+        v.push_back(s);
     }
-    cin >> k;
-    for(int i=0; i<k; i++){
-        cin >> num >> dir;
-        num--;
-        if(num - 1 >= 0 && wheels[num][6] != wheels[num-1][2]){
-        rotate_left(num-1, dir * -1);
-        }
-        if(num + 1 < t && wheels[num][2] != wheels[num+1][6]){
-        rotate_right(num+1, dir * -1);
+    cin >> K;
+    for(int i=0; i<K; i++){
+        cin >> a >> b; a--;
+        memset(visited, 0, sizeof(visited));
+        rotate_(a, b);
+        // cout << "---------" << '\n';
     }
-        if(dir == 1)rotate(wheels[num].rbegin(), wheels[num].rbegin() + 1, wheels[num].rend());
-        else rotate(wheels[num].begin(), wheels[num].begin() + 1, wheels[num].end());
-    }
-    for(int i=0; i<t; i++){
-        if(wheels[i][0] == 1)ret++;
+    for(int i=0; i<T; i++){
+        if(v[i][0] == '1')ret++;
     }
     cout << ret << '\n';
     return 0;
