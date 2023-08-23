d6e68de2c6d8f94f1a4c0ad94fd9b2f44ca82bed
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-08-24 00:15:02 +0900

    [Gold IV] Title: 알파벳, Time: 400 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,33 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m, visited[30], dy[4] = {-1, 0, 1, 0}, dx[4] = {0, 1, 0, -1}, ret;
+int n, m, dy[4] = {-1, 1, 0, 0}, dx[4] = {0, 0, -1, 1}, ret, visited[24][24];
 char ar[24][24];
+string s;
+int ar2[24][24];
 
-
-void dfs(int y, int x, int cnt) {
-    ret = max(cnt, ret);
+void go(int y, int x, int now, int cnt){
+    // cout << now << '\n';
+    ret = max(ret, cnt);
+    if(ret == 26)return;
     for(int i=0; i<4; i++){
         int ny = y + dy[i];
         int nx = x + dx[i];
         if(ny < 0 || nx < 0 || ny >= n || nx >= m)continue;
-        if(visited[(int)ar[ny][nx] - 'A'])continue;
-        
-        visited[(int)ar[ny][nx] - 'A'] = 1;
-        dfs(ny,nx,cnt+1);
-        visited[(int)ar[ny][nx] - 'A'] = 0;
+        if(now & (1<<ar[ny][nx] - 'A'))continue;
+        if(visited[ny][nx])continue;
+        visited[ny][nx] = 1;
+        go(ny, nx, now | (1 << ar[ny][nx] - 'A'), cnt+1);
+        visited[ny][nx] = 0;
     }
 }
-int main()
-{
+
+int main() {
     cin >> n >> m;
     for(int i=0; i<n; i++){
+        cin >> s;
         for(int j=0; j<m; j++){
-            cin >> ar[i][j];
+            ar[i][j] = s[j];
         }
     }
-    visited[(int)ar[0][0] - 'A'] = 1; 
-    dfs(0,0,1);
+
+    visited[0][0] = 1;
+    int temp = (1<<ar[0][0]-'A');
+
+    go(0,0, temp, 1);
     cout << ret << '\n';
     return 0;
 }
