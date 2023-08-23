66724128b4148191b66f7a74fc882df59cb0d794
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-08-23 23:07:08 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 4 ms, Memory: 4496 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -1,35 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MAX = 200000; 
-int visited[MAX+4];
-long long cnt[MAX+4];
+typedef long long ll;
+
+const int INF = 200000;
+int su, si, visited[INF+4];
+ll cnt[INF+4];
+
+
 int main() {
-    int n, m;
-    cin >> n >> m;
-    if(n == m){
-        puts("0"); puts("1");
-        return 0; 
-    } 
-    visited[n] = 1;
-    cnt[n] = 1;
+    cin >> su >> si;
+    if(su == si){
+        cout << 0 << '\n';
+        cout << 1 << '\n';
+        return 0;
+    }
     queue<int> q;
-    q.push(n);
-    while (!q.empty()) {
-        int now = q.front();
-        q.pop();
-        for (int next : {now-1, now+1, now*2}) {
-            if (0 <= next && next <= MAX) { 
-                if (!visited[next]) {
-                    q.push(next); 
-                    visited[next] = visited[now] + 1;
-                    cnt[next] += cnt[now];
-                } else if (visited[next] == visited[now] + 1) {
-                    cnt[next] += cnt[now];
-                }
+    q.push(su);
+    visited[su] = 1;
+    cnt[su] = 1;
+    while(q.size()){
+        int now = q.front(); q.pop();
+        
+        for(int next : {now+1, now-1, now*2}){
+            if(0 <= next && next <= INF){
+            if(!visited[next]){
+                visited[next] = visited[now] + 1;
+                cnt[next] += cnt[now];
+                q.push(next);
+            }else if(visited[next] == visited[now]+1){
+                cnt[next] += cnt[now];
             }
         }
+        }
+        
     }
-    cout << visited[m] - 1 << '\n';
-    cout << cnt[m] << '\n';
+    cout << visited[si]-1 << '\n';
+    cout << cnt[si] << '\n';
     return 0;
 }
