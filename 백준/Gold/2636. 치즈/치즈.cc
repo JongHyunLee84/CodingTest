aeabc6ab436b17c84eafade9979deb6250d84fb1
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-08-24 22:30:10 +0900

    [Gold IV] Title: 치즈, Time: 0 ms, Memory: 2148 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.cc
+++ 백준/Gold/2636. 치즈/치즈.cc
@@ -1,56 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m, t, ret, ar[104][104], visited[104][104], dy[4] = {-1, 0, 1, 0}, dx[4] = {0, 1, 0, -1};
-
-void dfs(int y, int x)
-{
-    if(ar[y][x] == 1){
-        ar[y][x] = 0;
-        visited[y][x] = 1;
-        return;
-    }
-    visited[y][x] = 1;
-    for(int i=0; i<4; i++){
-        int ny = dy[i] + y;
-        int nx = dx[i] + x;
-        if(ny<0 || nx<0 || ny >=n || nx>=m || visited[ny][nx])continue;
-        dfs(ny, nx);
-    }
-}
-int main()
-{
+int n, m, ar[104][104], visited[104][104], temp[104][104], dy[4] = {-1, 0, 1, 0}, dx[4] = {0, 1, 0, -1}, times, cheese;
+pair<int,int> start = {0,0};
+int main() {
     cin >> n >> m;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
             cin >> ar[i][j];
+            temp[i][j] = ar[i][j];
         }
     }
-    while(true)
-    {
+    while(1){
+        // cout << "here" << '\n';
         memset(visited, 0, sizeof(visited));
-        int cnt = 0;
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                if (ar[i][j] == 1)
-                    cnt++;
+        queue<pair<int,int>> q;
+        q.push(start);
+        visited[0][0] = 1;
+        int isZero = 1, temp_chese = 0;
+        while(q.size()){
+            auto now = q.front(); q.pop();
+            for(int i=0; i<4; i++){
+                int ny = dy[i] + now.first;
+                int nx = dx[i] + now.second;
+                if(ny >= n || nx >= m || ny < 0 || nx < 0)continue;
+                if(visited[ny][nx])continue;
+                if(ar[ny][nx] == 1){
+                    temp[ny][nx] = 0;
+                    visited[ny][nx] = 1;
+                    isZero = 0;
+                    temp_chese++;
+                    continue;
+                }
+                visited[ny][nx] = 1;
+                q.push({ny,nx});
             }
         }
-
-        if (cnt == 0)
-            break;
-        else
-        {
-            dfs(0,0);
-            t++;
-        }
-        ret = cnt;
+        if(isZero)break;
+        memcpy(ar, temp, sizeof(temp));
+        cheese = temp_chese;
+        times++;
     }
-    cout << t << '\n';
-    cout << ret << '\n';
+    cout << times << '\n' << cheese << '\n';
+    
     return 0;
 }
+
