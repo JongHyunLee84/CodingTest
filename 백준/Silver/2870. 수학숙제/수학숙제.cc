fac8c27fd774706247cd0a3bd64af50a1088135d
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-08-24 16:29:55 +0900

    [Silver IV] Title: 수학숙제, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/2870. 수학숙제/수학숙제.cc
+++ 백준/Silver/2870. 수학숙제/수학숙제.cc
@@ -1,48 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int n;
-string s, temp = "";
+string s;
 vector<string> v;
 
-bool compare(string a, string b)
-{
-    if (a.size() == b.size())
+bool comp(string a, string b){
+    if(a.size() == b.size()){
         return a < b;
+    }
     return a.size() < b.size();
 }
 
-int main()
-{
-    cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> s;
-        for (auto i : s)
-        {
-            if (isdigit(i))
-            {
-                temp += i;
-            }
-            else
-            {
-                if (temp != "")
-                {
+
+int main() {
+        cin >> n;
+        for(int i=0; i<n; i++){
+            cin >> s;
+            string temp = "";
+            for(int j=0; j<s.size(); j++){
+                if(s[j] - '0' >= 0 && s[j] -'0' <= 9){
+                    if(temp == "0"){
+                        temp = "";
+                    }
+                    temp += s[j];
+                }else if(temp != ""){
                     v.push_back(temp);
                     temp = "";
                 }
             }
-            if (temp.size() == 2 && temp[0] == '0')
-                temp = temp[1];
+            if(temp.size())v.push_back(temp);
         }
-        if (temp != "")
-        {
-            v.push_back(temp);
-            temp = "";
-        }
-    }
-
-    sort(v.begin(), v.end(), compare);
-    for (string i : v)
-        cout << i << '\n';
+        sort(v.begin(), v.end(), comp);
+        for(string i : v)cout << i << '\n';
     return 0;
 }
