a7269c3e86e3adefe142f5d2800c2d5a02d2895f
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-08-24 15:03:51 +0900

    [Gold V] Title: AC, Time: 36 ms, Memory: 3112 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.cc
+++ 백준/Gold/5430. AC/AC.cc
@@ -1,46 +1,73 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int T, n;
-string P, s;
+int t, n;
+string s, ar;
 
-int main(){
-    cin >> T;
-    for(int z=0; z<T; z++){
-        cin >> P >> n >> s;
-        deque<int> d;
-        int temp = 0;
-        for(int i=0; i<s.size(); i++){
-            if(s[i] == '[' || s[i] == ']')continue;
-            else if(s[i] == ',' && temp != 0){
-                d.push_back(temp);
-                temp=0;
-            }
-            else temp = temp * 10 + (s[i]-'0');
-        }
-        if(temp != 0)d.push_back(temp);
+// deque<int> split(string s, char del){
+//     deque<int> de;
+    
+//     while(1){
+//         auto pos = s.find(del);
+//         if(pos == string::npos)break;
         
-        bool rev = 0, error = 0;
-        for(int i=0; i<P.size(); i++){
-            if(P[i]=='R') rev = !rev;
+//         int temp = stoi(s.substr(0, pos));
+//         de.push_back(temp);
+//         s.erase(s.begin(), s.begin() + pos + 1);
+//     }
+//     if(s.size())de.push_back(stoi(s));
+//     // for(int i : li)cout << i << ' ';
+//     // cout << '\n';
+//     return de;
+// }
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+    cin >> t;
+    for(int z=0; z<t; z++){
+        cin >> s >> n >> ar; ar.erase(ar.begin()); ar.erase(ar.end()-1);
+        int isFront = 1, isBreak = 0;
+        
+        deque<int> de;
+        
+        int temp = 0;
+        for(int i=0; i<ar.size(); i++){
+            if(ar[i] == ',' && temp){
+                de.push_back(temp);
+                temp = 0;
+            }else {
+                temp = temp * 10 + (ar[i]-'0');
+            }
+        }
+        if(temp)de.push_back(temp);
+        
+        for(int i=0; i<s.size(); i++){
+            if(s[i] == 'R')isFront ^= 1;
             else {
-                if(d.size()==0) {
-                    error = 1;
+                if(!de.size()){
+                    isBreak = 1;
+                    cout << "error" << '\n';
                     break;
                 }
-                if(rev)d.pop_back();
-                else d.pop_front();
+                if(isFront)de.pop_front();
+                else de.pop_back();
             }
         }
-        if(rev)reverse(d.begin(), d.end());
-        if(error) cout << "error" <<'\n';
-        else {
+        if(isBreak) continue;
+        if(de.size() == 0){
+            cout << "[]" << '\n';
+            continue;
+        }
+        if(isFront){
             cout << '[';
-            for(int i=0; i<d.size(); i++){
-             if(i==d.size()-1)cout << d[i];
-             else cout << d[i] << ',';
-            }
-            cout << ']' << '\n';
+            for(int i=0; i<de.size()-1; i++)cout << de[i] << ',';
+            cout << de.back() << ']';
+            cout << '\n';
+        }else {
+            cout << '[';
+            for(int i=de.size()-1; i >=1; i--)cout << de[i] << ',';
+            cout << de.front() << ']';
+            cout << '\n';
         }
     }
     return 0;
