153dfdc180efb2aa2843e3a135878e26426dbc14
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-08-25 01:46:41 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.cc
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.cc
@@ -1,39 +1,42 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int k, n, a[104], visited[104], cnt;
-const int INF = 987654321;
-vector<int> v;
-int main(){
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> k >> n;
-    for(int i = 0; i < n; i++) cin >> a[i];
-    for(int i=0; i<n; i++){
-        if(!visited[a[i]]){
-            if(v.size() == k){
-                int last_idx = 0, pos;
-                for(int _a : v){
-                    int here_pick = INF;
-                    for(int j = i+1; j<n; j++){
-                        if(_a == a[j]){
-                            here_pick = j;
-                            break;
+
+int n, k, ar[104], visited[104], ret;
+
+int main() {
+    cin >> n >> k;
+    for(int i=0; i<k; i++){
+        cin >> ar[i];
+    }
+    vector<int> v;
+    for(int i=0; i<k; i++){
+        // cout << "here" << '\n';
+        if(visited[ar[i]])continue;
+        else {
+            if(v.size() == n){
+                int last_idx = 0, pos; 
+                for(int now : v){
+                    int next_idx = 1e9;
+                    for(int j=i+1; j<k; j++){
+                        if(now == ar[j]){
+                            next_idx = j; break;
                         }
                     }
-                    if(last_idx < here_pick){
-                        last_idx = here_pick;
-                        pos = _a;
-                        
+                    if(last_idx < next_idx){
+                        last_idx = next_idx;
+                        pos = now;
                     }
                 }
-                
-                // cout << a[i] << '.' << pos << '\n';
-                cnt++;
-                visited[pos] = 0;
                 v.erase(find(v.begin(), v.end(), pos));
+                // cout << pos << '\n';
+                visited[pos] = 0;
+                ret++;
             }
-            v.push_back(a[i]); visited[a[i]] = 1;
+            v.push_back(ar[i]);
+            visited[ar[i]] = 1;
         }
     }
-    cout << cnt << "\n";
+    cout << ret << '\n';
     return 0;
 }
+
