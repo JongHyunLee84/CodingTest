a8b365e1de76efb3227bddd8625d410cf98e26b9
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-08-24 15:56:44 +0900

    [Gold IV] Title: 문자열 폭발, Time: 88 ms, Memory: 7796 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
@@ -1,34 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-string s, boom, ret;
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL); 
-    cin >> s >> boom ;
-    int ss = boom.size();
-    stack<char> st;
-    for(char i : s) {
-        st.push(i);
-        if(st.size() >= ss && st.top() == boom[ss-1]){
-            string temp = "";
-            for(auto j : boom)temp+=st.top(), st.pop();
-            reverse(temp.begin(),temp.end());
-            if(temp != boom){
-                for(auto j : temp)st.push(j);
+string s, bom, ret;
+
+int main() {
+        cin >> s >> bom;
+        for(int i=0; i<s.size(); i++){
+            ret += s[i];
+            if(ret.size() >= bom.size() && ret.substr(ret.size() - bom.size(), bom.size()) == bom){
+                ret.erase(ret.end()-bom.size(), ret.end());   
             }
         }
-    }
-    if(st.size()){
-        while(st.size()){
-            ret += st.top();
-            st.pop();
-        }
-        reverse(ret.begin(), ret.end());
-        cout << ret << '\n';
-    }
-    else cout << "FRULA" << '\n';
+        if(ret.size())cout << ret << '\n';
+        else cout << "FRULA" << '\n';
     return 0;
 }
