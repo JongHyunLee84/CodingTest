f1b7b554877d5a5ecf99015ddbfc8cfb998cbf92
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-08-25 02:23:06 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.cc
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.cc
@@ -1,12 +1,19 @@
- #include <bits/stdc++.h>
- using namespace std;
- 
- int n, ret = 2;
- int main() {
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, ret = 2;
+
+int main() {
     cin >> n;
-    while(n--){
+    for(int i=0; i<n; i++){
         ret = ret * 2 - 1;
     }
     cout << ret * ret << '\n';
     return 0;
- }
+}
+
+// 1 -> 3
+// 2 -> 5
+// 3 -> 9
+// 4 -> 17
+// 5 -> 33
