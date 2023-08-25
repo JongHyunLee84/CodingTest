e05f27d714e7e39325dda0f3f15852515cec7a4c
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-08-25 21:40:51 +0900

    [Gold V] Title: 다이어트, Time: 8 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/19942. 다이어트/다이어트.cc
+++ 백준/Gold/19942. 다이어트/다이어트.cc
@@ -1,85 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int INF = 987654321;
-int n, mp, mf, ms, mv, temp, ret = INF;
-int ar[19][5];
-vector<vector<int>> v, rv[7500];
-vector<int> lv;
 
-// bool compare(vector<int> a, vector<int> b){
-//     if(a.size() == b.size()) return a > b;
-//     else {
-//         if(a.size() > b.size()){
-//             for(int i=0; i<b.size(); i++){
-//                 return a[i] > b[i];
-//             }
-//         }
-//         else if(a.size() < b.size()){
-//             for(int i=0; i<a.size(); i++){
-//                 return a[i] > b[i];
-//             }
-//         }
-//     }
-// }
-void combi(int start, vector<int> b) {
-    if(b.size() == temp){
-        v.push_back(b);
-        return;
+int n, ret = 1e9, mp, mf, ms, mv;
+struct Ingre {
+    int mp, mf, ms, mv, ci;
+};
+Ingre ar[19];
+vector<vector<int>> total;
+
+int sum_(vector<int> v){
+    int result = 0;
+    for(int i : v){
+        result += ar[i].ci;
     }
-    
-    for(int i=start+1; i<n; i++){
-        b.push_back(i);
-        combi(i, b);
-        b.pop_back();
-    }
+    return result;
 }
 
-int main() {
+bool check(vector<int> v){
+    int tp = 0, tf = 0, ts = 0, tv = 0;
+    for(int i : v){
+        tp += ar[i].mp; tf += ar[i].mf; ts += ar[i].ms; tv += ar[i].mv;
+    }
+    if(tp >= mp && tf >= mf && ts >= ms && tv >= mv)return true;
+    else return false;
+}
+
+void go(int idx, vector<int> v){
+    // if(idx > 8) return;
+    // cout << idx << ' ' << n << '\n';
+    if(idx == n){
+        // cout << "30" << '\n';
+        if(check(v)){
+        int result = sum_(v);
+        if(ret > result){
+            // cout << result << '\n';
+            // for(int i : v)cout << i << ' ';
+            // cout << '\n';
+            ret = result;
+            total.clear();
+            total.push_back(v);
+        }else if(ret == result){
+            total.push_back(v);
+        }
+        }
+        return;
+    }
+    go(idx+1, v);
+    v.push_back(idx);
+    go(idx+1, v);
+}
+
+int main()
+{
     cin >> n >> mp >> mf >> ms >> mv;
     for(int i=0; i<n; i++){
-        cin >> ar[i][0] >> ar[i][1] >> ar[i][2] >> ar[i][3] >> ar[i][4];
+        cin >> ar[i].mp >> ar[i].mf >> ar[i].ms >> ar[i].mv >> ar[i].ci;
     }
-    vector<int> b;
-    for(int i=1; i<=n; i++){
-        temp = i;
-        combi(-1,b);
+    vector<int> v;
+    go(0, v);
+    // cout << "here" << '\n';
+    sort(total.begin(), total.end());
+    if(ret == 1e9)cout << -1 << '\n';
+    else {
+    cout << ret << '\n';
+    for(int i : total[0])cout << i+1 << ' ';
     }
-    for(auto i : v){
-        int pi = 0, fi = 0, si= 0, vi=0, ci=0;
-        vector<int> a;
-        for(auto j : i){
-            a.push_back(j);
-            pi += ar[j][0];
-            fi += ar[j][1];
-            si += ar[j][2];
-            vi += ar[j][3];
-            ci += ar[j][4];
-        }
-        if(pi >= mp && fi >= mf && si >= ms && vi >= mv) {
-            // cout<< pi << '.' << fi << '.' << si << '.' << vi << '.' << ci << '\n';
-            // for(int i : a) cout << i << ' ';
-            // cout << '\n';
-            ret = min(ret, ci);
-            if(ret == ci)rv[ret].push_back(a);
-        }
-    }
-    // for(auto i:rv[ret]){
-    //     for(auto j:i) cout << j << ' ';
-    //     cout << '\n';
-    // }
-    // sort(lv.begin(), lv.end());
-    if(ret > 7500){
-        cout << -1 << '\n';
-    }else {
-        sort(rv[ret].begin(), rv[ret].end());
-        cout << ret << '\n';
-        for(int i:rv[ret][0]) cout << i+1 << ' ';
-    }
-    // for(auto i:v) {
-    //     for(auto j:i){
-    //         cout << j << ' ';
-    //     }
-    //     cout << '\n';
-    // }
     return 0;
 }
