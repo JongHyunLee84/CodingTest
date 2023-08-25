3a3c249e0a8e5ccb12ea36cff5ef460695f74f11
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-08-26 01:12:39 +0900

    [Gold III] Title: 순회강연, Time: 44 ms, Memory: 2196 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.cc
+++ 백준/Gold/2109. 순회강연/순회강연.cc
@@ -1,34 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, p, d, ret, max_d;
+int n, a, b, max_d, ret, visited[10004];
 vector<pair<int,int>> v;
-priority_queue<int, vector<int>, greater<int>> pq;
+
+bool comp(pair<int,int> a, pair<int,int> b){
+    if(a.first == b.first){
+        return a.second < b.second;
+    }return a.first > b.first;
+}
+
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL); 
-    cin >> n;
-    if(n==0){
-        cout << 0 <<'\n'; return 0;
-    }
+    cin >> n; 
     for(int i=0; i<n; i++){
-        cin >> p >> d;
-        v.push_back({d, p});
+        cin >> a >> b;
+        max_d = max(max_d, b);
+        v.push_back({a,b});
     }
-    sort(v.begin(), v.end());
+    sort(v.begin(), v.end(), comp);
     for(int i=0; i<n; i++){
-        pq.push(v[i].second);
-        if(pq.size() > v[i].first){
-            pq.pop();
+        if(!visited[v[i].second])visited[v[i].second] = v[i].first;
+        else {
+            for(int j=v[i].second; j >= 1; j--){
+                if(!visited[j]){visited[j] = v[i].first; break;}
+            }
         }
     }
-    while(pq.size()) {
-        ret += pq.top();
-        pq.pop();
+    for(int i=1; i<=max_d; i++){
+        ret += visited[i];
+        // cout << visited[i] << ' ';
     }
-    
     cout << ret << '\n';
     return 0;
 }
