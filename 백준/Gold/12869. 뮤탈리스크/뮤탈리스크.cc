66b5e431ff95a12cb25b45720b79978c2076b201
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-08-25 19:40:14 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 0 ms, Memory: 3048 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
@@ -1,41 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, visited[64][64][64], a[3];
-int ar[6][3] = {
-    {9,3,1},
-    {9,1,3},
-    {3,9,1},
-    {3,1,9},
-    {1,3,9},
-    {1,9,3}
+int n, visited[64][64][64];
+vector<int> v(3);
+int attack[6][3] = {
+    {9, 3, 1},
+    {9, 1, 3},
+    {3, 9, 1},
+    {3, 1, 9},
+    {1, 9, 3},
+    {1, 3, 9}
 };
-queue<vector<int>> q;
+struct SCV {
+    int a, b, c;
+};
+queue<SCV> q;
 
-int main() {
-    cin >> n;
-    for(int i=0; i<n; i++){
-        cin >> a[i];
-    }
-    q.push({a[0],a[1],a[2]});
-    visited[a[0]][a[1]][a[2]] = 1;
+void solve(int a, int b, int c){
+    SCV temp; temp.a = a; temp.b = b; temp.c = c;
+    q.push(temp);
+    visited[a][b][c] = 1;
+    
     while(q.size()){
+        SCV now = q.front(); q.pop();
         if(visited[0][0][0])break;
-        int ta = q.front()[0];
-        int tb = q.front()[1];
-        int tc = q.front()[2];
-        // cout << ta << ' ' << tb << ' '<< tc << '\n';
-        q.pop();
         for(int i=0; i<6; i++){
-            int ta_ = max(0,ta - ar[i][0]);
-            int tb_ = max(0,tb - ar[i][1]);
-            int tc_ = max(0,tc - ar[i][2]);
-            if(visited[ta_][tb_][tc_])continue;
-            visited[ta_][tb_][tc_] = visited[ta][tb][tc] + 1;
-            q.push({ta_, tb_, tc_});
+            int na = max(0, now.a - attack[i][0]);
+            int nb = max(0, now.b - attack[i][1]);
+            int nc = max(0, now.c - attack[i][2]);
+            if(visited[na][nb][nc])continue;
+            visited[na][nb][nc] = visited[now.a][now.b][now.c] + 1;
+            q.push({na,nb,nc});
         }
     }
-    cout << visited[0][0][0]-1 << '\n';
-    
+}
+
+int main()
+{
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> v[i];
+    }
+    solve(v[0], v[1], v[2]);
+    cout << visited[0][0][0] - 1 << '\n';
+
     return 0;
 }
