2304295988f55ce3cc181a5122ba01148f717efb
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-08-26 02:35:00 +0900

    [Gold IV] Title: 연구소, Time: 60 ms, Memory: 5344 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -1,92 +1,94 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int ret, n, m, a[10][10], temp[10][10], visited[10][10], dy[4] = {-1, 0, 1, 0}, dx[4] = {0, 1, 0, -1};
-vector<vector<pair<int, int>>> vv;
-vector<pair<int, int>> v;
-
-void dfs(int y, int x)
-{
-    temp[y][x] = 2;
-    visited[y][x] = 1;
-    for (int i = 0; i < 4; i++)
-    {
-        int ny = y + dy[i];
-        int nx = x + dx[i];
-        if (ny < 0 || ny >= n || nx < 0 || nx >= m)
-        {
-            continue;
-        }
-        if (visited[ny][nx] == 1 || temp[ny][nx] != 0)
-        {
-            continue;
-        }
-
-        dfs(ny, nx);
+int n, m, ar[12][12], visited[12][12], temp_ar[12][12], dy[4] = {-1, 0, 1, 0}, dx[4] = {0, -1, 0, 1}, ret;
+vector<vector<int>> vv;
+vector<pair<int, int>> blank;
+void combi(int idx, vector<int> v){
+    if(v.size() == 3){
+        vv.push_back(v);
+        return;
+    }
+    for(int i=idx+1; i<blank.size(); i++){
+        v.push_back(i);
+        combi(i, v);
+        v.pop_back();
     }
 }
 
-int main()
-{
+void print() {
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cout << visited[i][j] << ' ' ;
+        }
+        cout << '\n';
+    }
+    cout << '\n';
+}
 
-    cin >> n >> m;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            cin >> a[i][j];
-            if (a[i][j] == 0)
-            {
-                v.push_back({i, j});
-            }
+void count_(){
+    int sum = 0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            if(!visited[i][j])sum++;
         }
     }
-    int s = v.size();
-    for (int i = 0; i < s; i++)
-    {
-        for (int j = i + 1; j < s; j++)
-        {
-            for (int k = j + 1; k < s; k++)
-            {
+    // if(sum == 6)print();
+    ret = max(ret, sum);
+}
 
-                int cnt = 0;
-                for (int i = 0; i < n; i++)
-                {
-                    for (int j = 0; j < m; j++)
-                    {
-                        temp[i][j] = a[i][j];
-                        visited[i][j] = 0;
-                    }
-                }
+void dfs(int y, int x){
+    for(int i=0; i<4; i++){
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if(ny < 0 || nx < 0 || ny >= n || nx >= m)continue;
+        if(visited[ny][nx])continue;
+        visited[ny][nx] = 2;
+        temp_ar[ny][nx] = 1;
+        dfs(ny,nx);
+    }
+}
 
-                temp[v[i].first][v[i].second] = 1;
-                temp[v[j].first][v[j].second] = 1;
-                temp[v[k].first][v[k].second] = 1;
-
-                // dfs
-                for (int i = 0; i < n; i++)
-                {
-                    for (int j = 0; j < m; j++)
-                    {
-                        if (temp[i][j] == 2 && visited[i][j] == 0)
-                        {
-                            dfs(i, j);
-                        }
-                    }
-                }
-
-                for (int i = 0; i < n; i++)
-                {
-                    for (int j = 0; j < m; j++)
-                    {
-                        if (temp[i][j] == 0)
-                            cnt++;
-                    }
-                }
-                if (cnt >= ret)
-                    ret = cnt;
-            }
+void spread() {
+    memset(temp_ar, 0, sizeof(temp_ar));
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            if(visited[i][j] == 2){temp_ar[i][j] = 1; dfs(i,j);}
         }
+    }
+}
+
+
+
+
+int main()
+{
+    cin >> n >> m;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin >> ar[i][j];
+            if(!ar[i][j])blank.push_back({i,j});
+        }
+    }
+    vector<int> v;
+    combi(-1, v);
+    // for(auto i : vv){
+    //     for(int j : i){
+    //         cout << j << ' ';
+    //     }
+    //     cout << '\n';
+    // }
+    for(int i=0; i<vv.size(); i++){
+        memcpy(visited, ar, sizeof(visited));
+        for(int j : vv[i]){
+            auto temp = blank[j];
+            visited[temp.first][temp.second] = 1;
+        }
+        spread();
+        count_();
+        
+        // print();
+        // cout << '\n';
     }
     cout << ret << '\n';
     return 0;
