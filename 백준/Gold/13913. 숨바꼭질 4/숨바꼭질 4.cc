d31107ea77a4cbdca0afe4131952a2d1aa78bf75
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-08-29 19:24:59 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 16 ms, Memory: 3588 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
@@ -1,54 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int max_ = 100004;
-int n, k, ar[max_], visited[max_];
-queue<int> q;
-vector<int> v;
-int main()  {
-    cin >> n >> k;
-    q.push(n);
-    ar[n] = 1;
-    visited[n] = n;
-    
-    while(q.size()) {
-        int now = q.front();
-        // cout << now << ':';
-        //     for(int i : visited[now]) {
-        //         cout << i << ' ';
-        //     }
-        //     cout << '\n';
-        q.pop();
-        for(int next : {now+1, now-1, now*2}) {
-            if( 0 <= next && next <= 100000 ){
-                if(!ar[next]){
-                    ar[next] = ar[now] + 1;
-                    q.push(next);
-                    visited[next] = now;
-                
-                } 
-                // if(next == k) {
-                //     cout << ar[k]-1 << '\n';
-                //     for(int i : visited[k]){
-                //         cout << i << ' ';
-                //     }
-                //     exit(0);
-                // }
-                // else if(ar[next] == ar[now] + 1){
-                //     visited[next] += 1;
-                // }
-            }
-        }
-        // visited[now] = {};
-    }
-    cout << ar[k]-1 << '\n';
-    int temp = k;
-    for(int i=0; i<ar[k]-1; i++){
-        v.push_back(visited[temp]);
-        temp = visited[temp];
-    }
-    reverse(v.begin(),v.end());
-    v.push_back(k);
-    for(int i : v) cout << i << ' ';
-    return 0;    
-}
 
+int visited[100004],route[100004], a, b;
+queue<int> q;
+int main()
+{
+    cin >> a >> b;
+    if(a == b){
+        cout << 0 << '\n';
+        cout << a << '\n';
+        return 0;
+    }
+    q.push(a);
+    visited[a] = 1;
+    while(q.size()){
+        int now = q.front(); q.pop();
+        if(now == b)break;
+        for(int next : {now+1, now-1, now*2}){
+            if(next < 0 || next > 100000) continue;
+            if(visited[next])continue;
+            q.push(next);
+            visited[next] = visited[now] + 1;
+            route[next] = now;
+        }
+    }
+    cout << visited[b]-1 << '\n';
+    vector<int> v;
+    v.push_back(b);
+    while(1){
+        if(route[b]){v.push_back(route[b]); b = route[b];}
+        else break;
+    }
+    if(a == 0)v.push_back(a);
+    for(int i=v.size()-1; i>=0; i--)cout << v[i] << ' ';
+    return 0;
+}
