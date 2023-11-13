918638cd735709e70ae3d89517830135b522696a
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-11-13 23:56:04 +0900

    [level 2] Title: 가장 큰 수, Time: 0.12 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.swift
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.swift
@@ -2,9 +2,6 @@ import Foundation
 
 func solution(_ numbers:[Int]) -> String { 
     let ret = numbers.sorted { num1, num2 -> Bool in
-        if num1 == 0 {
-            return false
-        }
         var str1 = String(num1)
         var str2 = String(num2)
         var case1 = Int(str1 + str2)!
