9dd56597d958efccd049ef01092d0abe1577b4f9
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-11-21 00:31:34 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.swift
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.swift
@@ -7,16 +7,8 @@ func solution(_ n:Int, _ a:Int, _ b:Int) -> Int
     var ret = 0
     while aa ^ 1 != bb ^ 1 {
         ret += 1
-        if aa % 2 == 0 {
-            aa /= 2
-        } else {
-            aa = (aa + 1) / 2
-        }
-        if bb % 2 == 0 {
-            bb /= 2
-        } else {
-            bb = (bb + 1) / 2
-        }
+        aa = (aa + 1) / 2
+        bb = (bb + 1) / 2
     }
     return ret
 }
